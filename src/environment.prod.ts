75fe25ba1639b970001877dca371b201ba1a0ced
tjbarrett42 <tjbarrett42@gmail.com>
2023-09-21 20:27:00 -0400

    env fix 2

---
+++ src/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    openMeteoApiKey: 'tU9Zk9YSzmTTV6kZ',
+    anotherSecret: 'another-secret-here'
+  };
---
+++ src/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    openMeteoApiKey: 'tU9Zk9YSzmTTV6kZ',
+    anotherSecret: 'another-secret-here'
+  };
